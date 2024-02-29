961bf86519bba6ba9bc524f64778a30532ae0f9f
Gabriel_Soto <g.soto4792@gmail.com>
2024-02-29 08:12:41 -0600

    Añadidas pruebas unitarias
    Cambios a la clase AppTest.java

--- HT4/src/test/java/uvg/edu/gt/AppTest.java
+++ HT4/src/test/java/uvg/edu/gt/AppTest.java
@@ -9,7 +9,79 @@ import junit.framework.TestSuite;
  */
 public class AppTest 
     extends TestCase
-{
+{ 
+    public void testConvertirInfixAPostfix() {
+        Expresion e = new Expresion("3 + ( 4 * 5 ) - 6");
+        assertEquals("3 4 5 * + 6 -", e.getPostfix());
+
+        e = new Expresion("( ( 1 + 2 ) * ( 3 + 4 ) ) * 5");
+        assertEquals("1 2 + 3 4 + * 5 *", e.getPostfix());
+
+        e = new Expresion("1 + 2 * ( 3 - 4 ) / 5");
+        assertEquals("1 2 3 4 - * 5 / +", e.getPostfix());
+
+        e = new Expresion("1 + 2 * ( 3 - 4 ) ^ 5");
+        assertEquals("1 2 3 4 - 5 ^ * +", e.getPostfix());
+
+        e = new Expresion("( ( 1 + 2 ) ^ ( 3 + 4 ) ) * 5");
+        assertEquals("1 2 + 3 4 + ^ * 5 *", e.getPostfix());
+    }
+
+    public void testEvaluar() {
+        Expresion e = new Expresion("3 + ( 4 * 5 ) - 6");
+        assertEquals(13, e.evaluar());
+
+        e = new Expresion("( ( 1 + 2 ) * ( 3 + 4 ) ) * 5");
+        assertEquals(150, e.evaluar());
+
+        e = new Expresion("1 + 2 * ( 3 - 4 ) / 5");
+        assertEquals(3.2, e.evaluar());
+
+        e = new Expresion("1 + 2 * ( 3 - 4 ) ^ 5");
+        assertEquals(243.0, e.evaluar());
+
+        e = new Expresion("( ( 1 + 2 ) ^ ( 3 + 4 ) ) * 5");
+        assertEquals(33750.0, e.evaluar());
+
+        e = new Expresion("10 + 20 * ( 30 - 40 ) / 5");
+        assertEquals(-290.0, e.evaluar());
+
+        e = new Expresion("10 + 20 * ( 30 - 40 ) ^ 5 / 5");
+        assertEquals(2.936012937e12, e.evaluar());
+    }
+
+    public void testInvalidInfix() {
+        try {
+            Expresion e = new Expresion("1 + 2 ) * 3");
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            // expected
+        }
+
+        try {
+            Expresion e = new Expresion("1 + 2 * ( 3 - 4 ) ) * 5");
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            // expected
+        }
+
+        try {
+            Expresion e = new Expresion("1 + 2 * ( 3 - 4 ) ) ^ 5 * 5");
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException expected) {
+            // expected
+        }
+    }
+
+    public void testDivisionByZero() {
+        try {
+            Expresion e = new Expresion("1 + 2 * ( 3 - 4 ) / 0");
+            fail("Expected ArithmeticException");
+        } catch (ArithmeticException expected) {
+            // expected
+        }
+    }
+    
     /**
      * Create the test case
      *
@@ -36,3 +108,4 @@ public class AppTest
         assertTrue( true );
     }
 }
+
